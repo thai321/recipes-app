34b85053a0bfc64d8e76134a73b7a0d986f25a8d
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-05 01:12:23 -0700

    add admin feature

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,8 @@
 class ChefsController < ApplicationController
   before_action :set_chef, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  # before_action :require_admin, only: [:destroy]
+  # only admin can delete a chef account, but user can delete their own account
 
   def new
     @chef = Chef.new
@@ -40,6 +42,7 @@ class ChefsController < ApplicationController
   def destroy
     @chef.destroy
     flash[:danger] = "#{@chef.name.capitalize} was deleted successfully"
+    session[:chef_id] = nil if !current_chef.admin?
     redirect_to chefs_path
   end
 
@@ -53,10 +56,17 @@ class ChefsController < ApplicationController
       params.require(:chef).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def require_same_user
-      if current_chef != @chef
+    def require_same_user         # alow admin to access
+      if current_chef != @chef and !current_chef.admin?
         flash[:danger] = "You can only edit or delete your own account"
         redirect_to current_chef
       end
     end
+
+    # def require_admin  # only admin can delete a chef account
+    #   if logged_in? and !current_chef.admin?
+    #     flash[:danger] = "Only admin can perform this delete action"
+    #     redirect_to root_path
+    #   end
+    # end
 end
